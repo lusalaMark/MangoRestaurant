f9d54fc9239c21bed7334c72d97275ad710b8b52
lusalamark <lusalamark63@gmail.com>
2023-04-10 18:58:36 +0300

    Product  Repository Interface

---
+++ MangoServices.ProductAPI/Models/Dto/ProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MangoServices.ProductAPI.Models.Dto
+{
+    public class ProductDto
+    { 
+        public int MyProperty { get; set; } 
+        public string Name { get; set; } 
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ MangoServices.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using MangoServices.ProductAPI.Models.Dto;
+
+namespace MangoServices.ProductAPI.Repository
+{
+    public  interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
+    }
+}
